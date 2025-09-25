dc303e62f4072379a841a7070a290ceefeed6daa
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-09-25 23:03:06 +0530

    nested routing

--- app/courses/page.js
+++ app/courses/page.js
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <h2 className="text-2xl font-semibold tracking-tight">Courses Page</h2>
+
+      <div className="flex items-center justify-center gap-5"> 
+        <Link href="/courses/cohort1">Cohort-1</Link>
+        <Link href="/courses/cohort2">Cohort-2</Link>
+        <Link href="/courses/cohort3">Cohort-3</Link>
+      </div>
     </div>
   );
 };
