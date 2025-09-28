300a0e05174b9d0a62b06e77cb8446e0db0e309b
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-09-28 14:53:36 +0530

    not-found page in next.js

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold tracking-tight text-center pt-20">Not Found Page</h2>
+    </div>
+  );
+};
+
+export default notFound;
